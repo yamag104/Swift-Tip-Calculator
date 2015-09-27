9cf6cfe4644b254d2bf51a00fe323f3247959fc2
Yoko Yamaguchi <yamag104@mail.chapman.edu>
2015-09-26 18:09:33 -0700

    Added more UI including UISliders, Buttons, and calculation methods

--- SwiftTipCalculator/ViewController.swift
+++ SwiftTipCalculator/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var billTextfield : UITextField = UITextField()
+    var alertLabel : UILabel = UILabel()
+    var taxLabel : UILabel = UILabel()
+    var sliderLabel : UILabel = UILabel()
+    var percent = 0.0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.makeLayout()
@@ -19,21 +25,60 @@ class ViewController: UIViewController {
     func makeLayout() {
         let screenSize: CGRect = UIScreen.mainScreen().bounds
         let screenWidth : CGFloat = screenSize.width
-        let screenHeight : CGFloat = screenSize.height
+        //let screenHeight : CGFloat = screenSize.height
         let labelWidth : CGFloat = 200
         let labelHeight : CGFloat = 20
         
-        /* Hello Swift Label */
-        let helloLabel = UILabel(frame: CGRectMake((screenWidth / 2) - (labelWidth / 2), ((screenHeight / 2) - (labelHeight / 2)), labelWidth, labelHeight))
+        /* Tip Calculator Label */
+        let helloLabel = UILabel(frame: CGRectMake((screenWidth / 2) - (labelWidth / 2), 60, labelWidth, labelHeight))
         helloLabel.textAlignment = NSTextAlignment.Center
-        helloLabel.text = "Hello Swift!"
+        helloLabel.text = "Tip Calculator"
         self.view.addSubview(helloLabel)
         
-        /* Name Label */
-        let nameLabel = UILabel(frame: CGRectMake((screenWidth / 2) - (labelWidth / 2), 350, 200, 20))
-        nameLabel.textAlignment = NSTextAlignment.Center;
-        nameLabel.text = "Yoko Yamaguchi"
-        self.view.addSubview(nameLabel)
+        /* Bill Label */
+        let billLabel = UILabel(frame: CGRectMake((screenWidth / 2) - (labelWidth / 2), 130, 200, 20))
+        billLabel.textAlignment = NSTextAlignment.Center;
+        billLabel.text = "Total Bill:"
+        self.view.addSubview(billLabel)
+        
+        /* Bill Textfield */
+        billTextfield.frame = CGRectMake((screenWidth / 2) - (labelWidth / 2), 150, 200, 20)
+        billTextfield.borderStyle = UITextBorderStyle.Line
+        self.view.addSubview(billTextfield)
+        
+        /* Slider Label */
+        sliderLabel.frame = CGRectMake((screenWidth / 2) - (labelWidth / 2), 180, 200, 20)
+        sliderLabel.textAlignment = NSTextAlignment.Center;
+        sliderLabel.text = "Tax percentage"
+        self.view.addSubview(sliderLabel)
+        
+        /* Tip Slider */
+        let tipSlider = UISlider(frame: CGRectMake((screenWidth / 2) - (labelWidth / 2), 210, 200, 20))
+        tipSlider.minimumValue = 0
+        tipSlider.maximumValue = 100
+        tipSlider.addTarget(self, action:"sliderValueDidChange:", forControlEvents: UIControlEvents.ValueChanged)
+        self.view.addSubview(tipSlider)
+        
+        /* Calculate Button */
+        let calculateButton = UIButton(type:UIButtonType.System) as UIButton
+        calculateButton.frame = CGRectMake((screenWidth / 2) - (labelWidth / 2), 270, 200, 20)
+        calculateButton.backgroundColor = UIColor.whiteColor()
+        calculateButton.setTitle("Calculate Tax", forState: UIControlState.Normal)
+        calculateButton.addTarget(self, action:"calculate:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(calculateButton)
+        
+        /* Alert Label */
+        alertLabel.frame = CGRectMake((screenWidth / 2) - (labelWidth / 2), 300, 200, 20)
+        alertLabel.textAlignment = NSTextAlignment.Center;
+        alertLabel.text = ""
+        alertLabel.textColor = UIColor.redColor()
+        self.view.addSubview(alertLabel)
+        
+        /* Tax Label */
+        taxLabel.frame = CGRectMake((screenWidth / 2) - (labelWidth / 2), 300, 200, 20)
+        taxLabel.textAlignment = NSTextAlignment.Center;
+        taxLabel.text = ""
+        self.view.addSubview(taxLabel)
     }
     
     override func didReceiveMemoryWarning() {
@@ -41,6 +86,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func calculate(sender:UIButton!) {
+        validateNumeric()
+        let result = calculateTax(Double(billTextfield.text!)!, percentage: (percent))
+        taxLabel.text = NSString(format: "Your tax is $%.2f", result) as String
+    }
+    
+    func validateNumeric() {
+        alertLabel.text = ""
+        // toInt returns optional b/c of Int?
+        let billTotal = Double(billTextfield.text!)
+        // check a and b before unwrapping using !
+        if ((billTotal) != nil) {
+            NSLog("Numerical")
+            //var ans = a! + b!
+            //answerLabel.text = "Answer is \(ans)"       // answerLabel ie UILa
+        } else {
+            NSLog("Not numerical")
+            alertLabel.text = "Bill has to be numeric"
+            self.view.addSubview(alertLabel)
+        }
+    }
+    
+    func sliderValueDidChange(sender:UISlider!) {
+        sliderLabel.text = NSString(format: "%.1f%%", sender.value) as String
+        percent = Double(sender.value)
+        NSLog(sliderLabel.text!)
+        
+    }
+    
+    func calculateTax(totalBill:Double, percentage:Double) -> Double {
+        return (totalBill * percentage * 0.01)
+    }
+    
+    
 
 }
 
